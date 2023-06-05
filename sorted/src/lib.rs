b31e25a67b81024ddcdb6d80dca9fffcb5b2a34c
Longtao Zhang <longtao.zlt@alibaba-inc.com>
2023-06-05 20:42:12 +0800

    feat(sorted): 实现第一个挑战的第二个版本
    Signed-off-by: Longtao Zhang <longtao.zlt@alibaba-inc.com>

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,10 +1,18 @@
 use proc_macro::TokenStream;
-use syn::__private::quote::quote;
+use syn::{
+    DeriveInput, Item,
+    __private::{quote::quote, ToTokens},
+    parse_macro_input,
+};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    let input = Item::from(input);
+    if let Item::Enum(data) = input {
+        return data.to_token_stream().into();
+    }
 
     quote! {}.into()
 }
