1dd5fcec48758a7cd811dde8178b659d44b19a74
Longtao Zhang <longtao.zlt@alibaba-inc.com>
2023-06-05 21:32:08 +0800

    feat(sorted): 完成第三关
    Signed-off-by: Longtao Zhang <longtao.zlt@alibaba-inc.com>

--- main.rs
+++ main.rs
@@ -1,28 +1,22 @@
-// Ensure that your macro reports a reasonable error message when the caller
-// mistypes the inert attribute in various ways. This is a compile_fail test.
-//
-// The preferred way to report an error from a procedural macro is by including
-// an invocation of the standard library's compile_error macro in the code
-// emitted by the procedural macro.
-//
-//
-// Resources:
-//
-//   - The compile_error macro for emitting basic custom errors:
-//     https://doc.rust-lang.org/std/macro.compile_error.html
-//
-//   - Lowering a syn::Error into an invocation of compile_error:
-//     https://docs.rs/syn/1.0/syn/struct.Error.html#method.to_compile_error
+// This test is similar to the previous where want to ensure that the macro
+// correctly generates an error when the input enum is out of order, but this
+// time it is using an enum that also has data associated with each variant.
 
-use derive_builder::Builder;
+use sorted::sorted;
 
-#[derive(Builder)]
-pub struct Command {
-    executable: String,
-    #[builder(each = "arg")]
-    args: Vec<String>,
-    env: Vec<String>,
-    current_dir: Option<String>,
+use std::env::VarError;
+use std::error::Error as StdError;
+use std::fmt;
+use std::io;
+use std::str::Utf8Error;
+
+#[sorted]
+pub enum Error {
+    Fmt(fmt::Error),
+    Io(io::Error),
+    Utf8(Utf8Error),
+    Var(VarError),
+    Dyn(Box<dyn StdError>),
 }
 
 fn main() {}
--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,7 @@
 use proc_macro::TokenStream;
 use proc_macro2;
 use syn::{
-    DeriveInput, Item,
-    __private::{quote::quote, ToTokens},
-    parse_macro_input, Error,
+    DeriveInput, Item, __private::ToTokens, parse_macro_input, spanned::Spanned, Error, Variant,
 };
 
 #[proc_macro_attribute]
@@ -12,7 +10,23 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let input = Item::from(input);
     match input {
-        Item::Enum(data) => data.to_token_stream().into(),
+        Item::Enum(data) => {
+            let source: Vec<_> = data
+                .variants
+                .iter()
+                .map(|item| (item, item.ident.to_string()))
+                .collect();
+            let mut sorted = source.clone();
+            sorted.sort_by(|a, b| a.1.cmp(&b.1));
+            for (a, b) in source.iter().zip(sorted.iter()) {
+                if a.1 != b.1 {
+                    return Error::new(b.0.span(), format!("{} should sort before {}", b.1, a.1))
+                        .to_compile_error()
+                        .into();
+                }
+            }
+            data.to_token_stream().into()
+        }
         _ => Error::new(
             proc_macro2::Span::call_site(),
             "expected enum or match expression",
