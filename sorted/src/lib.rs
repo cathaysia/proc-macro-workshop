4e7e86f7bd8b1919eead4841a5678be6a69387be
Longtao Zhang <longtao.zlt@alibaba-inc.com>
2023-06-05 21:12:08 +0800

    feat(sorted): 完成第二个挑战
    Signed-off-by: Longtao Zhang <longtao.zlt@alibaba-inc.com>

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,8 +1,9 @@
 use proc_macro::TokenStream;
+use proc_macro2;
 use syn::{
     DeriveInput, Item,
     __private::{quote::quote, ToTokens},
-    parse_macro_input,
+    parse_macro_input, Error,
 };
 
 #[proc_macro_attribute]
@@ -10,9 +11,13 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
     let input = parse_macro_input!(input as DeriveInput);
     let input = Item::from(input);
-    if let Item::Enum(data) = input {
-        return data.to_token_stream().into();
+    match input {
+        Item::Enum(data) => data.to_token_stream().into(),
+        _ => Error::new(
+            proc_macro2::Span::call_site(),
+            "expected enum or match expression",
+        )
+        .to_compile_error()
+        .into(),
     }
-
-    quote! {}.into()
 }
